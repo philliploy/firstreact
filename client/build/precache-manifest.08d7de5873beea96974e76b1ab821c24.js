df6a8569f6fe00ce150b57644488dde9bcd6a567
Phillip Loy <ploy3_98@yahoo.com>
2019-03-07 00:21:38 -0600

    deployed

---
+++ client/build/precache-manifest.08d7de5873beea96974e76b1ab821c24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36bf16f3e10b69cc7604",
+    "url": "/firstreact/static/js/runtime~main.33501035.js"
+  },
+  {
+    "revision": "51b95edf8d66d716a92a",
+    "url": "/firstreact/static/js/main.23da7187.chunk.js"
+  },
+  {
+    "revision": "ff661de6195320cfc0eb",
+    "url": "/firstreact/static/js/2.822769df.chunk.js"
+  },
+  {
+    "revision": "51b95edf8d66d716a92a",
+    "url": "/firstreact/static/css/main.9c5bf012.chunk.css"
+  },
+  {
+    "revision": "709211bca04adb1718e45c1252b8c6b1",
+    "url": "/firstreact/index.html"
+  }
+];
