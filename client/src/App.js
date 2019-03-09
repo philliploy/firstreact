438d9a51b61c40d101acd133ebd329a5357d073d
Phillip Loy <ploy3_98@yahoo.com>
2019-03-09 02:25:51 -0600

    deployed

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
-
+ 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
